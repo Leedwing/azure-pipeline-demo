03a8e5190ca2a20d914c216d762c3b5f6ee15b6d
lee-edwingnguepedja-tchwangnwou <79*=PMdnsLmla4>
2019-10-30 16:31:37 +0100

    change to test CI on Azure with passed tests

--- src/main/java/com/oncode/azurepipelinedemo/controller/HelloController.java
+++ src/main/java/com/oncode/azurepipelinedemo/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String index() {
-        return "Implementing an Azure Pipeline!";
+        return "Implementing an Azure Pipeline";
     }
 }

79ceba436bb57f078837a447dfdbbd7ca3f9b23e
lee-edwingnguepedja-tchwangnwou <79*=PMdnsLmla4>
2019-10-30 15:32:35 +0100

    first commit

---
+++ src/test/java/com/oncode/azurepipelinedemo/AzurePipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oncode.azurepipelinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurePipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

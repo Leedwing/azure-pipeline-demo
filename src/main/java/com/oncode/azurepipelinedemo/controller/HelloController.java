f34399b7556e85068834b4b01e1a7175cdee41b3
lee-edwingnguepedja-tchwangnwou <79*=PMdnsLmla4>
2019-10-31 11:10:57 +0100

    changed the properties file

--- src/main/java/com/oncode/azurepipelinedemo/controller/HelloController.java
+++ src/main/java/com/oncode/azurepipelinedemo/controller/HelloController.java
@@ -1,12 +1,17 @@
 package com.oncode.azurepipelinedemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${welcome.string}")
+    private String welcomeMessage;
+
     @RequestMapping("/")
     public String index() {
-        return "Implementing an Azure Pipeline";
+        return welcomeMessage;
     }
 }
